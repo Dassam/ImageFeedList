5ce2b872a4d7dca559c1a58077720f17800161da
Dassam <dassam@yandex.ru>
2023-06-10 19:40:07 +0300

    #8 - Made the screen according to the layout - Implemented the logic needed to display 20 images along with likes - Implemented Gradient on date - Added the necessary icons

--- ImageFeedList/Presentation/ImagesList/ImagesListCell.swift
+++ ImageFeedList/Presentation/ImagesList/ImagesListCell.swift
@@ -8,5 +8,22 @@
 import UIKit
 
 final class ImagesListCell: UITableViewCell {
-
+    @IBOutlet var cellImageView: UIImageView!
+    @IBOutlet var dateLabel: UILabel!
+    @IBOutlet var likeButton: UIButton!
+    @IBOutlet var dateBackground: UIView!
+    
+    static let reuseIdentifier = "ImagesListCell"
+    
+    let gradient = CAGradientLayer()
+    let gradientColors = [
+        UIColor(red: 0.102,
+                green: 0.106,
+                blue: 0.133,
+                alpha: 0.2).cgColor,
+        UIColor(red: 0.102,
+                green: 0.106,
+                blue: 0.133,
+                alpha: 0).cgColor
+      ]
 }
--- ImageFeedList/Presentation/ImagesList/ImagesListViewController.swift
+++ ImageFeedList/Presentation/ImagesList/ImagesListViewController.swift
@@ -8,104 +8,77 @@
 import UIKit
 
 class ImagesListViewController: UIViewController {
-    
     @IBOutlet private var tableView: UITableView!
     
-    override var preferredStatusBarStyle: UIStatusBarStyle {
-        return UIStatusBarStyle.lightContent
-    }
+    private let photosName: [String] = Array(0..<20).map{ "\($0)" }
+    private lazy var dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .long
+        formatter.timeStyle = .none
+        return formatter
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
-        // Do any additional setup after loading the view.
+        tableView.contentInset = UIEdgeInsets(top: 12, left: 0, bottom: 12, right: 0)
     }
+    
+    private func configCell(for cell: ImagesListCell, with indexPath: IndexPath) {
+        
+        guard let image = UIImage(named: photosName[indexPath.row]) else { return }
+        
+        cell.cellImageView.image = image
+        
+        cell.dateLabel.text = dateFormatter.string(from: Date())
+        
+        cell.gradient.frame = cell.dateBackground.bounds
+        cell.gradient.colors = cell.gradientColors
+        cell.gradient.locations = [0, 1]
+        cell.gradient.startPoint = CGPoint(x: 0.25, y: 0.5)
+        cell.gradient.endPoint = CGPoint(x: 0.75, y: 0.5)
+        
+        cell.dateBackground.layer.addSublayer(cell.gradient)
+        
+        if indexPath.row % 2 != 0 {
+            cell.likeButton.setImage(UIImage(named: "favoritesActive"), for: .normal)
+        } else {
+            cell.likeButton.setImage(UIImage(named: "favoritesNoActive"), for: .normal)
+        }
+    }
+    
 }
-
 // MARK: Working with tableView
 
 extension ImagesListViewController: UITableViewDataSource, UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return photosName.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
-    }
-   
-    /*func numberOfSections (in tableView: UITableView) -> Int { 1 }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let player = players[indexPath.row]
-        
-        guard let cell = playersTableView.dequeueReusableCell (withIdentifier: cellIdentifier,
-                                                               for: indexPath) as? PlayersTableViewCell else {
+        let cell = tableView.dequeueReusableCell(withIdentifier: ImagesListCell.reuseIdentifier, for: indexPath)
+
+        guard let imageListCell = cell as? ImagesListCell else {
             return UITableViewCell()
         }
-        
-        cell.configure(
-            with: PlayersTableViewCell.PlayersNameModel(
-                textField: player.name
-            )
-        )
-        return cell
+        configCell(for: imageListCell, with: indexPath)
+        return imageListCell
     }
     
-    //------------------------- Удаление игрока ------------------------//
-    func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
-        return .delete
-    }
-    
-    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            
-            let playerToRemove = self.players[indexPath.row]
-            self.context.delete(playerToRemove)
-            do {
-                try self.context.save()
-            } catch {
-                print("Error Message: Couldn't delet player name.")
-            }
-            self.fetchPlayers()
-            
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        }
-    }
-    
-    //------------------------- Изменение имени игрока ------------------------//
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        tableView.deselectRow(at: indexPath, animated: false)
-        let player = self.players[indexPath.row]
-        
-        let alert = UIAlertController(title: errorMessages.renamePlayerMessage(), message: nil, preferredStyle: .alert)
-        alert.addTextField() { (playerTF) in
-            playerTF.text = player.name
-        }
-        
-        alert.addAction(UIAlertAction(title: "Изменить", style: .default, handler: { _ in
-            let input = alert.textFields?.first?.text?.trimmingCharacters(in: .whitespacesAndNewlines)
-            
-            if (input != "") {
-                guard let name = input else { return }
-                
-                player.name = name
-                
-                do {
-                    try self.context.save()
-                } catch {
-                    print("Error Message: Couldn't edit player name.")
-                }
-                self.fetchPlayers()
-            } else {
-                self.showAlert(self.errorMessages.nonEmptyPlayerNameMessage())
-            }
-        }))
-        
-        alert.addAction(UIAlertAction(title: "Закрыть", style: .default))
-        
-        self.present(alert, animated: true, completion: nil)
-    }*/
-}
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
 
+      guard let image = UIImage(named: "\(indexPath.row)") else {
+        preconditionFailure("Image Not Found")
+      }
+      let imageInsets = UIEdgeInsets(top: 4, left: 16, bottom: 4, right: 16)
+      let imageViewWidth = tableView.bounds.width - imageInsets.left - imageInsets.right
+      let imageWidth = image.size.width
+      let scale = imageViewWidth / imageWidth
+      let imageHeight = image.size.height * scale
+      let imageViewHeight = imageHeight + imageInsets.top + imageInsets.bottom
+      return imageViewHeight
+    }
+    
+}
