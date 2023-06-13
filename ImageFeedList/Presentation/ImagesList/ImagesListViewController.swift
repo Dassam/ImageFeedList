e4551edbc87baf6b6c7fcde5ff8eae03ed7a9c8a
Dassam <dassam@yandex.ru>
2023-06-13 13:23:32 +0300

    #9 - Implemented the transfer of ImageData from ImagesListViewController to SingleImageViewController using an didSet method

--- ImageFeedList/Presentation/ImagesList/ImagesListViewController.swift
+++ ImageFeedList/Presentation/ImagesList/ImagesListViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 final class ImagesListViewController: UIViewController {
     @IBOutlet private var tableView: UITableView!
     
+    private let ShowSingleImageSegueIdentifier = "ShowSingleImage"
     private let photosName: [String] = Array(0..<20).map{ "\($0)" }
     private lazy var dateFormatter: DateFormatter = {
         let formatter = DateFormatter()
@@ -45,6 +46,17 @@ final class ImagesListViewController: UIViewController {
             cell.likeButton.setImage(UIImage(named: "like_button_off"), for: .normal)
         } else {
             cell.likeButton.setImage(UIImage(named: "like_button_on"), for: .normal)
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == ShowSingleImageSegueIdentifier {
+            let viewController = segue.destination as! SingleImageViewController
+            let indexPath = sender as! IndexPath
+            let image = UIImage(named: photosName[indexPath.row])
+            viewController.image = image
+        } else {
+            super.prepare(for: segue, sender: sender)
         }
     }
     
@@ -86,7 +98,7 @@ extension ImagesListViewController: UITableViewDataSource {
 //MARK: - UITableViewDataSource
 
 extension ImagesListViewController: UITableViewDelegate {
-    
-    
-    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: ShowSingleImageSegueIdentifier, sender: indexPath)
+    }
 }
--- ImageFeedList/Presentation/SingleImageView/SingleImageViewController.swift
+++ ImageFeedList/Presentation/SingleImageView/SingleImageViewController.swift
@@ -7,14 +7,31 @@
 
 import UIKit
 
-class SingleImageViewController: UIViewController {
+final class SingleImageViewController: UIViewController {
+    var image: UIImage! {
+        didSet {
+            guard isViewLoaded else { return }
+            imageView.image = image
+        }
+    }
     
-    @IBOutlet var imageView: UIImageView! 
-   
+    @IBOutlet var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        imageView.image = image
     }
     
 }
-
+/*
+ final class SingleImageViewController: UIViewController {
+     var image: UIImage!
+     
+     @IBOutlet var imageView: UIImageView!
+     
+     override func viewDidLoad() {
+         super.viewDidLoad()
+         imageView.image = image
+     }
+ }
+ */
