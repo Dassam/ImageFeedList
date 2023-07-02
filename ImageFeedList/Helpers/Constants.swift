932292a4af15bb44e4e22de3a2a181bf4aaf97db
Dassam <dassam@yandex.ru>
2023-07-02 14:06:13 +0300

    #9 - Removed spaces in SingleImageViewController and ProfileViewController - Created Constants file

---
+++ ImageFeedList/Helpers/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  ImageFeedList
+//
+//  Created by Dassam on 30.06.2023.
+//
+
+import UIKit
+
+enum Constant: String {
+    case accessKey = "BcUTlbzxNTJAURJiVMu6S-vjvn-i3eRVF85CIstrEYU"
+    case secretKey = "AceQ138eg17T1a0RHJ77eEE3UYGLJhu2ec9LGRELSAQ"
+    case redirectURI = "urn:ietf:wg:oauth:2.0:oob"
+    case accessScope = "public+read_user+write_likes"
+    case defaultBaseURL = "https://api.unsplash.com"
+}
+
--- ImageFeedList/Presentation/SingleImageView/SingleImageViewController.swift
+++ ImageFeedList/Presentation/SingleImageView/SingleImageViewController.swift
@@ -66,6 +66,3 @@ extension SingleImageViewController: UIScrollViewDelegate {
         imageView
     }
 }
-
-
-
